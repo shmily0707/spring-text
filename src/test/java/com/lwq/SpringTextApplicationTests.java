3e205434fed3dd5cc02249d45f4a638633516539
ASUS <kissfamilyli@163.com>
2020-03-16 18:42:58 +0800

    init

---
+++ src/test/java/com/lwq/SpringTextApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lwq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTextApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
